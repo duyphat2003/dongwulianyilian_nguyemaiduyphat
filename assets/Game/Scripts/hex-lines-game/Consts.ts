47236851ebfd4e8fc3655afdf3df07343a8baf42
duyphat2003 <rofduyphat2003@gmail.com>
2025-10-01 19:14:37 +0700

    Revert "update final"
    This reverts commit 8e1ac6fae2d35c36f259dce97112480961cef9d0.

--- assets/Game/Scripts/hex-lines-game/Consts.ts
+++
@@ -1,9 +0,0 @@
-export default class Consts
-{
-    static CenterY = 0;
-    static CenterX = 0;
-    static ColSize = 84;
-    static RowSize = 61;
-    static StagePageCount = 3;
-    static FreeSkinId = 5;
-}
--- assets/framework/network/MessageType.ts
+++
@@ -1,9 +0,0 @@
-/**
-* name 
-*/
-	export enum SocketTag {
-		KSOCKET_OPEN,
-		KSOCKET_CLOSE,
-		KSOCKET_MESSAGE,
-		KSOCKET_ERROR,
-	}	
